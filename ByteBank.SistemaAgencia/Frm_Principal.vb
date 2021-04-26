22c6161fe7a21901521f059cd232ea6d7c127ec2
Franciel Leandro <francielleandro@gmail.com>
2021-04-25 23:02:19 -0300

    iniciando projeto

---
+++ ByteBank.SistemaAgencia/Frm_Principal.vb
@@ -0,0 +1,14 @@
+﻿Public Class Frm_Principal
+    Public Sub New()
+
+        ' Esta chamada é requerida pelo designer.
+        InitializeComponent()
+
+        ' Adicione qualquer inicialização após a chamada InitializeComponent().
+
+        Lbl_Principal.Text = "Principal"
+        Me.Text = "Principal"
+        Btm_Principal.Text = "Clique Aqui"
+
+    End Sub
+End Class
